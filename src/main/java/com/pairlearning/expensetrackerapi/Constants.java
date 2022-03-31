e3d3d7e9c145c7c9cdd0f0db6b1030153d7e164a
gachuy <gachuy@canvia.com>
2022-03-31 08:27:09 -0500

    crud users

---
+++ src/main/java/com/pairlearning/expensetrackerapi/Constants.java
@@ -0,0 +1,11 @@
+package com.pairlearning.expensetrackerapi;
+
+public class Constants {
+
+    public static final String API_SECRECT_KEY = "expensetrackerapikey";
+
+    public static final long TOKEN_VALIDITY = 2 * 60 * 60 * 1000;
+
+
+
+}
